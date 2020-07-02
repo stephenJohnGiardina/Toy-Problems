06ebaf7f50c06a203fde81082ca65fe6cc76b2e6
stephenJohnGiardina <stephengia@yahoo.com>
2020-07-02 12:03:30 -0400

    Made some progress on addTwoNumbers

--- JavaScript/addTwoNumbers/addTwoNumbers.js
+++ JavaScript/addTwoNumbers/addTwoNumbers.js
@@ -15,22 +15,18 @@ let addTwoNumbers = function(l1, l2) {
   if (!l1 || !l2) {
     return null;
   }
-  let l1Number = 0;
-  let l2Number = 0;
+  let l1NumberString = '';
+  let l2NumberString = '';
   let l1Length = getLength(l1);
   let l2Length = getLength(l2);
-  let l1Power = Math.pow(10, getLength(l1) - 1);
-  let l2Power = Math.pow(10, getLength(l2) - 1);
+
   for (let i = 0; i < l1Length; i++) {
-    l1Number += removeTail(l1).val * l1Power;
-    l1Power /= 10;
+    l1NumberString += removeTail(l1).val;
   }
   for (let i = 0; i < l2Length; i++) {
-    l2Number += removeTail(l2).val * l2Power;
-    l2Power /= 10;
+    l2NumberString += removeTail(l2).val;
   }
-  let sum = JSON.stringify(l1Number + l2Number);
-  console.log(sum);
+  let sum = addTwoStrings(l1NumberString, l2NumberString);
   let result = new ListNode(Number.parseInt(sum[sum.length - 1]));
   let previousNode = result;
   if (sum.length > 1) {
@@ -76,8 +72,41 @@ let removeTail = (l) => {
   return current;
 }
 
-function linkedListToArray(linkedList) {
+function addTwoStrings(str1, str2) {
+  if (str1.length < 11 && str2.length < 11) {
+    return JSON.stringify(Number.parseInt(str1) + Number.parseInt(str2))
+  }
+  str1 = reverseString(str1);
+  str2 = reverseString(str2);
+  let longerLength = str1 < str2 ? str1.length : str2.length;
+  let result = '';
+  for (let i = longerLength - 1; i >= 0; i--) {
+    if (str1[i] === undefined) {
+      result += str2[i];
+    } else if (str2[i] === undefined) {
+      result += str1[i]
+    } else {
+      let nextDigit = Number.parseInt(str1[i]) +  Number.parseInt(str2[i])
+      if (nextDigit >= 10) {
+        if (result.length !== 0) {
+          newLastDigist = Number.parseInt(result[result.length - 1]) + 1;
+          result = result.substring(0, result.length - 1)
+          result += newLastDigist;
+          nextDigit = nextDigit - 10;
+        } else {
+          nextDigit = Number.parseInt(str1[i]) +  Number.parseInt(str2[i]);
+        }
+      }
+      result += nextDigit;
+    }
+  }
+  return result
+}
 
+function reverseString(str) {
+  let arr = str.split('');
+  let reverseArr = arr.reverse();
+  return reverseArr.join('');
 }
 
 function ListNode(val, next) {
@@ -103,9 +132,20 @@ let arrayToLinkedList = (array) => {
 
 // TEST 1
 
-
 console.log(addTwoNumbers(arrayToLinkedList([2, 4, 3]), arrayToLinkedList([5, 6, 4])))
 
-//TEST 2
+// TEST 2
+
+console.log(addTwoNumbers(arrayToLinkedList([5]), arrayToLinkedList([5])))
+
+// TEST 3
+
+console.log(addTwoNumbers(arrayToLinkedList([1,8]), arrayToLinkedList([0])))
+
+// TEST 4
 
 console.log(addTwoNumbers(arrayToLinkedList([1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1]), arrayToLinkedList([5,6,4])));
+
+// TEST 5
+
+console.log(addTwoNumbers(arrayToLinkedList([9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9]), arrayToLinkedList([1])));
