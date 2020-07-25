256ecffd43142357a4295528757af39aca371e95
stephenJohnGiardina <stephengia@yahoo.com>
2020-07-25 11:52:47 -0400

    Question 21 complete in JS & PY

--- 21/main.js
+++ 21/main.js
@@ -11,10 +11,82 @@
  * @return {ListNode}
  */
 var mergeTwoLists = function(l1, l2) {
-
+  l1 = linkedListToArray(l1);
+  l2 = linkedListToArray(l2);
+  let result = l1.concat(l2).sort((a, b) => a - b);
+  return arrayToLinkedList(result);
 };
 
 function ListNode(val, next) {
   this.val = (val === undefined ? 0: val)
   this.next = (next === undefined ? null: next)
 }
+
+function toString(head) {
+  let result = '';
+  while (head !== null) {
+    result += head.val + ' -> ';
+    head = head.next;
+  }
+  return result.substring(0, result.length - 4);
+}
+
+function linkedListToArray(head) {
+  let result = [];
+  while (head !== null) {
+    result.push(head.val);
+    head = head.next;
+  }
+  return result;
+}
+
+function arrayToLinkedList(array) {
+  if (array.length === 0) return null;
+  let current;
+  let head = new ListNode(array[0], null);
+  current = head;
+  for (let i = 1; i < array.length; i++) {
+    current.next = new ListNode(array[i], null);
+    current = current.next;
+  }
+  return head;
+}
+
+// TEST SUITE
+
+// TEST 1
+
+function test1() {
+  let node1 = new ListNode(1);
+  let node2 = new ListNode(2);
+  let node3 = new ListNode(4);
+  let node4 = new ListNode(1);
+  let node5 = new ListNode(3);
+  let node6 = new ListNode(4);
+  node1.next = node2;
+  node2.next = node3;
+  node4.next = node5;
+  node5.next = node6;
+  return mergeTwoLists(node1, node4);
+}
+
+console.log(toString(test1()));
+
+// TEST 2
+
+function test2() {
+  let node1 = new ListNode(-10);
+  let node2 = new ListNode(-6);
+  let node3 = new ListNode(-6);
+  let node4 = new ListNode(-6);
+  let node5 = new ListNode(-3);
+  let node6 = new ListNode(5);
+  node1.next = node2;
+  node2.next = node3;
+  node3.next = node4;
+  node4.next = node5;
+  node5.next = node6;
+  return mergeTwoLists(node1, null);
+}
+
+console.log(toString(test2()));
--- 21/main.py
+++ 21/main.py
@@ -1,7 +1,56 @@
 # Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
 class Solution:
     def mergeTwoLists(self, l1: ListNode, l2: ListNode) -> ListNode:
+        l1 = linkedListToArray(l1)
+        l2 = linkedListToArray(l2)
+        for i in range(len(l2)):
+            l1.append(l2[i])
+        return arrayToLinkedList(sorted(l1))
+
+def toString(head):
+    result = ''
+    while head != None:
+        result += str(head.val) + ' -> '
+        head = head.next
+    return result[0:len(result) - 4]
+
+def linkedListToArray(head):
+    result = []
+    while head != None:
+        result.append(head.val)
+        head = head.next
+    return result
+
+def arrayToLinkedList(array):
+    if len(array) == 0:
+        return None
+    head = ListNode(array[0], None)
+    current = head
+    for i in range(1, len(array)):
+        current.next = ListNode(array[i], None)
+        current = current.next
+    return head
+
+
+# TEST SUITE
+
+# TEST 1
+
+def test1():
+  node1 = ListNode(1)
+  node2 = ListNode(2)
+  node3 = ListNode(4)
+  node4 = ListNode(1)
+  node5 = ListNode(3)
+  node6 = ListNode(4)
+  node1.next = node2
+  node2.next = node3
+  node4.next = node5
+  node5.next = node6
+  return Solution.mergeTwoLists(None, node1, node4)
+
+print(toString(test1()))
