a21445ce2695a1476369628b2200e736d8677ea6
stephenJohnGiardina <stephengia@yahoo.com>
2020-08-04 10:29:52 -0400

    Question 36 initialized

---
+++ 36/main.js
@@ -0,0 +1,30 @@
+
+
+
+// TEST SUITE
+
+// TEST 1
+
+function test1() {
+  const actual;
+  const expected;
+  if (actual === expected) {
+    return '✔';
+  }
+  return 'X';
+}
+
+console.log(test1());
+
+// TEST 2
+
+function test2() {
+  const actual;
+  const expected;
+  if (actual === expected) {
+    return '✔';
+  }
+  return 'X';
+}
+
+console.log(test2());
