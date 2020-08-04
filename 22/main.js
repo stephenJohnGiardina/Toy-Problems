e382b6b805985519fcac700a8c8fe1761548143a
stephenJohnGiardina <stephengia@yahoo.com>
2020-08-04 12:44:49 -0400

    Question 22 initialized

---
+++ 22/main.js
@@ -0,0 +1,36 @@
+/**
+ * @param {number} n
+ * @return {string[]}
+ */
+var generateParenthesis = function(n) {
+
+};
+
+
+// TEST SUITE
+
+// TEST 1
+
+function test1() {
+  const actual;
+  const expected;
+  if (actual === expected) {
+    return '✔';
+  }
+  return 'X';
+}
+
+console.log(test1());
+
+// TEST 2
+
+function test2() {
+  const actual;
+  const expected;
+  if (actual === expected) {
+    return '✔';
+  }
+  return 'X';
+}
+
+console.log(test2());
---
+++ 22/main.py
@@ -0,0 +1,26 @@
+def generateParenthesis(n):
+
+
+# TEST SUITE
+
+# TEST 1
+
+def test1():
+  actual = None
+  expected = None
+  if (actual == expected):
+    return '✔'
+  return 'X'
+
+print(test1())
+
+# TEST 2
+
+def test2():
+  actual = None
+  expected = None
+  if (actual == expected):
+    return '✔'
+  return 'X'
+
+print(test2())
